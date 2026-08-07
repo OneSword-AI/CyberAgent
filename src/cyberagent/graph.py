e740af923c564b28707e9e3208b50b9dca13ea16
tuxnode <tuxnode@outlook.com>
2026-08-07 16:25:11 +0800

    feat: 将LLM分析题目信息加入到主逻辑中

--- src/cyberagent/graph.py
+++ src/cyberagent/graph.py
@@ -1,6 +1,6 @@
 from langgraph.graph import END, StateGraph
 
-from cyberagent.agents.classifier import classify_challenge
+from cyberagent.agents.llm_classifier import llm_classify_challenge
 from cyberagent.models import ChallengeState
 from cyberagent.providers import fetch_challenge
 
@@ -9,7 +9,7 @@ def build_graph():
     graph = StateGraph(ChallengeState)
 
     graph.add_node("fetch_challenge", fetch_challenge)
-    graph.add_node("classify_challenge", classify_challenge)
+    graph.add_node("classify_challenge", llm_classify_challenge)
 
     graph.set_entry_point("fetch_challenge")
     graph.add_edge("fetch_challenge", "classify_challenge")
