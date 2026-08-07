c22a9fb2127b623780d71e9fba1b02045ea5b6dd
tuxnode <tuxnode@outlook.com>
2026-08-07 19:29:16 +0800

    feat: 文件IO的tools封装

--- src/cyberagent/tools/__init__.py
+++ src/cyberagent/tools/__init__.py
@@ -1,6 +1,7 @@
 """Tool execution helpers."""
 
 from cyberagent.tools.executor import record_tool_output
+from cyberagent.tools.filesystem import inspect_file
 from cyberagent.tools.http import http_get, http_post, http_request
 from cyberagent.tools.models import ToolResult
 from cyberagent.tools.shell import run_shell
@@ -10,6 +11,7 @@ __all__ = [
     "http_get",
     "http_post",
     "http_request",
+    "inspect_file",
     "record_tool_output",
     "run_shell",
 ]
